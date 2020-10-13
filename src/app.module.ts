e20ec05a4b87b1b1da73109a422978db1621df0a
OhMinSeop <mins5190@gmail.com>
2020-10-14 00:29:24 +0900

    [Restaurant] ADD: Restaurant 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+    RestaurantsModule,
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/restaurants/restaurants.resolver.ts
@@ -0,0 +1,17 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateRestaurantDto } from './dtos/restaurant.dto';
+import { Restaurant } from './entities/restaurant.entity';
+
+@Resolver(of => Restaurant)
+export class RestaurantResolver {
+  @Query(type => [Restaurant])
+  restaurants(): Restaurant[] {
+    return [];
+  }
+
+  @Mutation(type => Boolean)
+  createRestaurant(@Args() createRestaurantDto: CreateRestaurantDto): boolean {
+    console.log(createRestaurantDto);
+    return true;
+  }
+}
